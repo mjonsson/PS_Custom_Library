0ffa0928f7a5d2a57b6a90ce0490878b7deb2a0c
mjonsson <smjonsson76@me.com>
2014-05-03 00:36:55 +0200

    Added support for Teamcenter 9

--- Handlers/ps_check_initiator_rh.cxx
+++ Handlers/ps_check_initiator_rh.cxx
@@ -34,6 +34,7 @@ int ps_check_initiator_rh(EPM_rule_message_t msg)
 			}
 		}
 		// Check if group of current user is defined
+#ifdef _TC10
 		if (args.getVec("current_group", valueVec))
 		{
 			tag_t		tGroupMember;
@@ -50,7 +51,26 @@ int ps_check_initiator_rh(EPM_rule_message_t msg)
 				itk(EMH_store_error_s1(EMH_severity_error, RULE_HANDLER_DEFAULT_IFAIL, c_ptr<char>().format("Process can only be initiated by group(s) '%s'.", concat_str(valueVec, ',', false).c_str())));
 			}
 		}
+#else
+		if (args.getVec("current_group", valueVec))
+		{
+			tag_t		tGroupMember;
+			tag_t		tGroup;
+			char		groupName[SA_name_size_c+1];
+
+			itk(SA_ask_current_groupmember(&tGroupMember));
+			itk(SA_ask_groupmember_group(tGroupMember, &tGroup));
+			itk(SA_ask_group_name(tGroup, groupName));
+
+			if (!find_str(groupName, valueVec))
+			{
+				decision = EPM_nogo;
+				itk(EMH_store_error_s1(EMH_severity_error, RULE_HANDLER_DEFAULT_IFAIL, c_ptr<char>().format("Process can only be initiated by group(s) '%s'.", concat_str(valueVec, ',', false).c_str())));
+			}
+		}
+#endif
 		// Check if role of current user is defined
+#ifdef _TC10
 		if (args.getVec("current_role", valueVec))
 		{
 			tag_t		tGroupMember;
@@ -67,6 +87,24 @@ int ps_check_initiator_rh(EPM_rule_message_t msg)
 				itk(EMH_store_error_s1(EMH_severity_error, RULE_HANDLER_DEFAULT_IFAIL, c_ptr<char>().format("Process can only be initiated by role(s) '%s'.", concat_str(valueVec, ',', false).c_str())));
 			}
 		}
+#else
+		if (args.getVec("current_role", valueVec))
+		{
+			tag_t		tGroupMember;
+			tag_t		tRole;
+			char		roleName[SA_name_size_c+1];
+
+			itk(SA_ask_current_groupmember(&tGroupMember));
+			itk(SA_ask_groupmember_role(tGroupMember, &tRole));
+			itk(SA_ask_role_name(tRole, roleName));
+
+			if (!find_str(roleName, valueVec))
+			{
+				decision = EPM_nogo;
+				itk(EMH_store_error_s1(EMH_severity_error, RULE_HANDLER_DEFAULT_IFAIL, c_ptr<char>().format("Process can only be initiated by role(s) '%s'.", concat_str(valueVec, ',', false).c_str())));
+			}
+		}
+#endif
 		// Check if full group of current user is defined
 		if (args.getVec("current_full_group", valueVec))
 		{
@@ -85,14 +123,15 @@ int ps_check_initiator_rh(EPM_rule_message_t msg)
 			}
 		}
 		// Check if any of users roles are defined
+#ifdef _TC10
 		if (args.getVec("current_effective_role", valueVec))
 		{
-			char		*userId = NULL;
-			tag_t		tUser,
-				tRole;
-			c_ptr<char>	roleName;
+			char			*userId = NULL;
+			tag_t			tUser;
+			tag_t			tRole;
+			c_ptr<char>		roleName;
 			c_ptr<tag_t>	groupMembers;
-			logical		roleFound = false;
+			logical			roleFound = false;
 
 			itk(POM_get_user_id(&userId));
 			itk(SA_find_user2(userId, &tUser));
@@ -116,6 +155,39 @@ int ps_check_initiator_rh(EPM_rule_message_t msg)
 				itk(EMH_store_error_s1(EMH_severity_error, RULE_HANDLER_DEFAULT_IFAIL, c_ptr<char>().format("Process can only be initiated by role(s) '%s'.", concat_str(valueVec, ',', false).c_str())));
 			}
 		}
+#else
+		if (args.getVec("current_effective_role", valueVec))
+		{
+			char			*userId = NULL;
+			tag_t			tUser;
+			tag_t			tRole;
+			char			roleName[SA_name_size_c+1];
+			c_ptr<tag_t>	groupMembers;
+			logical			roleFound = false;
+
+			itk(POM_get_user_id(&userId));
+			itk(SA_find_user(userId, &tUser));
+			itk(SA_find_groupmember_by_user(tUser, groupMembers.get_len_ptr(), groupMembers.get_ptr()));
+
+			for (int i = 0; i < groupMembers.get_len(); i++)
+			{
+				itk(SA_ask_groupmember_role(groupMembers.get(i), &tRole));
+				itk(SA_ask_role_name(tRole, roleName));
+
+				if (find_str(roleName, valueVec))
+				{
+					roleFound = true;
+					break;
+				}
+			}
+
+			if (!roleFound)
+			{
+				decision = EPM_nogo;
+				itk(EMH_store_error_s1(EMH_severity_error, RULE_HANDLER_DEFAULT_IFAIL, c_ptr<char>().format("Process can only be initiated by role(s) '%s'.", concat_str(valueVec, ',', false).c_str())));
+			}
+		}
+#endif
 	}
 	catch (tcexception& e)
 	{
